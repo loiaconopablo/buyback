4590ae679a8e23d578f1e58932bb798de2bcd3ee
rggrinberg <rggrinberg@d7edb913-66f0-4dd5-8174-52cfe1b22414>
2015-05-14 18:10:49 +0000

    git-svn-id: https://svn.riouxsvn.com/buyback@17 d7edb913-66f0-4dd5-8174-52cfe1b22414

--- trunk/src/protected/modules/admin_root/controllers/MigrationsController.php
+++ trunk/src/protected/modules/admin_root/controllers/MigrationsController.php
@@ -20,21 +20,25 @@ class MigrationsController extends Controller {
 	    echo htmlentities(ob_get_clean(), null, Yii::app()->charset);
 	}
 
-	public function actionUpdatePurchaseContractNumber()
-	{
-		$model = new Purchase();
+	/**
+	* Este metodo lo usamos para arreglar los numeros de contrato que no guardaban el punto de venta
+	* 14/05/2015
+	*/
+	// public function actionUpdatePurchaseContractNumber()
+	// {
+	// 	$model = new Purchase();
 
-		$model = $model->findAll('cae_response_json <> "Este es un CAI fijo temporal mientras se tramita el CAE"');
+	// 	$model = $model->findAll('cae_response_json <> "Este es un CAI fijo temporal mientras se tramita el CAE"');
 
-		foreach ($model as $purchase) {
+	// 	foreach ($model as $purchase) {
 
-			$contract_pdv_num = str_pad(124, 4, "0", STR_PAD_LEFT);
-			$contract_cn_num = str_pad($purchase->contract_number, 8, "0", STR_PAD_LEFT);
-			$final_contract_number = $contract_pdv_num . '-' . $contract_cn_num;
+	// 		$contract_pdv_num = str_pad(124, 4, "0", STR_PAD_LEFT);
+	// 		$contract_cn_num = str_pad($purchase->contract_number, 8, "0", STR_PAD_LEFT);
+	// 		$final_contract_number = $contract_pdv_num . '-' . $contract_cn_num;
 
-			$purchase->contract_number = $final_contract_number;
-//die(var_dump($purchase->contract_number));
-			$purchase->save();
-		}
-	}
+	// 		$purchase->contract_number = $final_contract_number;
+
+	// 		$purchase->save();
+	// 	}
+	// }
 }
