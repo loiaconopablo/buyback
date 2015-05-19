b15505e5596872dbacbb7dfa76f1048c29468d7d
Federico Juarez <federico.juarez@vafenet.com.ar>
2015-05-19 18:08:42 -0300

    Ordeno el repo para que tenga el orden de git

--- tags/beta.0.1/src/protected/views/default/message.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-block <?php echo $type; ?>">
-  <button type="button" class="close" data-dismiss="alert">&times;</button>
-    <?php echo urldecode($message); ?>
-</div>
--- tags/beta/src/protected/modules/retail/views/purchase/contract_wrap.php
+++
@@ -1,3 +0,0 @@
-<?php $this->renderPartial('contract', array('model' => $model, 'carrier_name' => $carrier_name, 'footer' => 'Original para BGH'));?>
-<?php $this->renderPartial('contract', array('model' => $model, 'carrier_name' => $carrier_name, 'footer' => 'Duplicado para la Sucursal'));?>
-<?php $this->renderPartial('contract', array('model' => $model, 'carrier_name' => $carrier_name, 'footer' => 'Triplicado para el Cliente'));?>
