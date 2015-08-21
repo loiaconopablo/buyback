dee373d7fc65a242292926e1f95afda000488949
Richard Grinberg <rggrinberg@gmail.com>
2015-08-20 22:08:22 -0400

    Evitando que el mismo dni venda el mismo imei

--- src/protected/modules/purchase/controllers/BuyController.php
+++ src/protected/modules/purchase/controllers/BuyController.php
@@ -312,6 +312,12 @@ class BuyController extends Controller
     {
 
         $model = new Purchase;
+        
+        if ($model->checkIsDuplicate(Yii::app()->session['purchase']['imei'], $seller->dni)) {
+            throw new Exception("El equipo ya fue comprado al mismo vendedor", 1);
+            
+            return;
+        }
 
         $point_of_sale = PointOfSale::model()->findByPk(Yii::app()->user->point_of_sale_id);
         $carrier = Carrier::model()->findByPk(Yii::app()->session['purchase']['carrier_id']);
