6477f9222a8cf48ef0a15b8e40768b1d62d39e1f
rggrinberg@gmail.com <rggrinberg@gmail.com>
2015-09-11 13:26:27 -0300

    Fix. se agrega empresa a create price list

--- src/protected/modules/admin/views/pricelist/_form.php
+++ src/protected/modules/admin/views/pricelist/_form.php
@@ -13,36 +13,40 @@
 	</p>
 
     <?php echo $form->errorSummary($model); ?>
+    <div>
+        <?php echo $form->labelEx($model, 'company_id'); ?>
+        <?php echo $form->dropDownList($model, 'company_id', GxHtml::listDataEx(Company::model()->findAll()), array('size' => TbHtml::INPUT_SIZE_XXLARGE, 'empty' => Yii::t('app', 'Seleccionar').'...')); ?>
+        <?php echo $form->error($model, 'company_id'); ?>
+    </div><!-- row -->
 
-				<div>
-    <?php echo $form->labelEx($model, 'brand'); ?>
-    <?php echo $form->textField($model, 'brand', array('maxlength' => 255)); ?>
-    <?php echo $form->error($model, 'brand'); ?>
-		</div><!-- row -->
-					<div>
-    <?php echo $form->labelEx($model, 'model'); ?>
-    <?php echo $form->textField($model, 'model', array('maxlength' => 255)); ?>
-    <?php echo $form->error($model, 'model'); ?>
-		</div><!-- row -->
-					<div>
-    <?php echo $form->labelEx($model, 'locked_price'); ?>
-    <?php echo $form->textField($model, 'locked_price', array('prepend' => '$', 'append' => '.00', 'class' => 'price')); ?>
-    <?php echo $form->error($model, 'locked_price'); ?>
-		</div><!-- row -->
-					<div>
-    <?php echo $form->labelEx($model, 'unlocked_price'); ?>
-    <?php echo $form->textField($model, 'unlocked_price', array('prepend' => '$', 'append' => '.00', 'class' => 'price')); ?>
-    <?php echo $form->error($model, 'unlocked_price'); ?>
-		</div><!-- row -->
-					<div>
-    <?php echo $form->labelEx($model, 'broken_price'); ?>
-    <?php echo $form->textField($model, 'broken_price', array('prepend' => '$', 'append' => '.00', 'class' => 'price')); ?>
-    <?php echo $form->error($model, 'broken_price'); ?>
-		</div><!-- row -->
+	<div>
+        <?php echo $form->labelEx($model, 'brand'); ?>
+        <?php echo $form->textField($model, 'brand', array('maxlength' => 255)); ?>
+        <?php echo $form->error($model, 'brand'); ?>
+	</div><!-- row -->
+	<div>
+        <?php echo $form->labelEx($model, 'model'); ?>
+        <?php echo $form->textField($model, 'model', array('maxlength' => 255)); ?>
+        <?php echo $form->error($model, 'model'); ?>
+	</div><!-- row -->
+	<div>
+        <?php echo $form->labelEx($model, 'locked_price'); ?>
+        <?php echo $form->textField($model, 'locked_price', array('prepend' => '$', 'append' => '.00', 'class' => 'price')); ?>
+        <?php echo $form->error($model, 'locked_price'); ?>
+	</div><!-- row -->
+	<div>
+        <?php echo $form->labelEx($model, 'unlocked_price'); ?>
+        <?php echo $form->textField($model, 'unlocked_price', array('prepend' => '$', 'append' => '.00', 'class' => 'price')); ?>
+        <?php echo $form->error($model, 'unlocked_price'); ?>
+	</div><!-- row -->
+	<div>
+        <?php echo $form->labelEx($model, 'broken_price'); ?>
+        <?php echo $form->textField($model, 'broken_price', array('prepend' => '$', 'append' => '.00', 'class' => 'price')); ?>
+        <?php echo $form->error($model, 'broken_price'); ?>
+	</div><!-- row -->
 							
 
 <?php
 echo TbHtml::submitButton(Yii::t('app', 'Guardar'), array('color' => TbHtml::BUTTON_COLOR_PRIMARY));
-$this->endWidget();
-?>
+$this->endWidget(); ?>
 </div><!-- form -->
