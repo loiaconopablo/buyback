9e8f401c853b73e5bd85b7eca90e46face706059
Renzo Belvedere <vagrant@localhost.localdomain>
2015-09-25 21:54:41 +0200

    Issue #102 - Validacion regex y confirmacion para baja masiva

--- src/protected/modules/admin/views/sellingcode/create.php
+++ src/protected/modules/admin/views/sellingcode/create.php
@@ -9,7 +9,7 @@ $this->menu = array(
     array('label' => Yii::t('app', 'Crear') . ' ' . $model->label(), 'icon' => 'plus-sign', 'url' => array('create')),
     array('label' => Yii::t('app', 'Listar') . ' ' . $model->label(2), 'icon' => 'list', 'url' => array('admin')),
     array('label' => Yii::t('app', 'Importar') . ' ' . $model->label(2), 'icon' => 'file', 'url' => array('upload')),
-    array('label' => Yii::t('app', 'Eliminar') . ' ' . $model->label(2), 'icon' => 'remove', 'url' => array('truncate')));
+    array('label' => Yii::t('app', 'Eliminar') . ' ' . $model->label(2), 'icon' => 'remove', 'url' => array('truncate'), 'linkOptions' => array('onClick'=>'return confirm("¿Desea eliminar todos los registros?");' )));
 ?>
 
 <?php
